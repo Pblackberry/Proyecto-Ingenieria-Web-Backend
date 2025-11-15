db5750c5961bf46dab8822825488ccced07c6054
Pblackberry <tomaslopezandrade2004@gmail.com>
2025-11-15 13:45:17 -0500

    Administrador de horarios

--- Controllers/Administrator/HorarioAdiministrator.py
+++ Controllers/Administrator/HorarioAdiministrator.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from Managers.db_manager import DbManager
-from Models.Horarios.TemporadaModel import Temporada
+from Models.Empleados.EmpleadosModel import Empleado
+from Models.Horarios import TemporadaModel, HorarioModel
 from Models.Mensajes.ReturnMessage import ReturnMessage
+from Controllers.Administrator.EmpleadoAdministrator import obtener_empleado
+from datetime import date, timedelta
 
 router = APIRouter(prefix="/admin/horario", tags=["Administración de horarios"])
 
 # Temporadas
 
 @router.post("/ingresar-temporada")
-async def ingresar_temporada(body: Temporada):
+async def ingresar_temporada(body: TemporadaModel.Temporada):
     conn = await DbManager.get_db_connection()
 
     try:
@@ -70,4 +73,70 @@ async def obtener_temporadas():
         return ReturnMessage(state=False, response_message=str(e))
     finally:
         conn.close
+
+# Horarios
+
+@router.post("/asignar-horario")
+async def asignar_horario(body: HorarioModel.HorarioRequest):
+    
+    if body.Fecha_inicio.weekday() != 0:
+        return ReturnMessage(state=False, response_message="Fecha de inicio invalida, el horario semanal debe comenzar en un lunes")
+    fecha_final = body.Fecha_inicio + timedelta(days=4)
+    conn = await DbManager.get_db_connection()
+    try:
+        async with conn.cursor() as cursor:
+            query = "EXEC sp_ObtenerEmpleadoKeyId ?"
+            await cursor.execute(query, body.Cedula_emplado)
+            row = await cursor.fetchone()
+            if row:
+                key_id = row[0]
+            else:
+                return ReturnMessage(state=False, response_message="No existe un empleado con la cedula proporcionada")
             
+            query = "EXEC sp_AsignarHorario ?, ?, ?, ?, ?, ?, ?, ?"
+            params = (body.Fecha_inicio, fecha_final, key_id, body.Horas_lunes, body.Horas_martes, body.Horas_miercoles, body.Horas_jueves, body.Horas_viernes)
+            await cursor.execute(query, params)
+            row = await cursor.fetchone()
+            if row:
+                sp_response = row[0]
+                if sp_response == 0:
+                    return ReturnMessage(state=True, response_message="Horario asignado con exito")
+                else:
+                    return ReturnMessage(state=False, response_message="Ya existe un horario asignado para este empleado en la semana seleccionada")
+            else:
+                return ReturnMessage(state=False, response_message="Error al ejecutar stored procedure")
+    except Exception as e:
+        return ReturnMessage(state=False, response_message=str(e))
+    finally:
+        conn.close()
+        
+@router.post("/cancelar-horario")
+async def cancelar_horario(body: HorarioModel.HorarioRequest):
+    if body.Fecha_inicio.weekday() != 0:
+        return ReturnMessage(state=False, response_message="Fecha de inicio invalida, el horario semanal debe comenzar en un lunes")
+    conn = await DbManager.get_db_connection()
+    try:
+        async with conn.cursor() as cursor:
+            query = "EXEC sp_ObtenerEmpleadoKeyId ?"
+            await cursor.execute(query, body.Cedula_emplado)
+            row = await cursor.fetchone()
+            if row:
+                key_id = row[0]
+            else:
+                return ReturnMessage(state=False, response_message="No existe un empleado con la cedula proporcionada")
+            query = "EXEC sp_EliminarHorario ?, ?"
+            params = (key_id, body.Fecha_inicio)
+            await cursor.execute(query, params)
+            row = await cursor.fetchone()
+            if row:
+                sp_response = row[0]
+                if sp_response == 0:
+                    return ReturnMessage(state=True, response_message="Horario eliminado con exito")
+                else:
+                    return ReturnMessage(state=False, response_message="No existe un horario asignado para este empleado en esta fecha")
+            else:
+                return ReturnMessage(state=False, response_message="Error al ejecutar stored procedure")
+    except Exception as e:
+        return ReturnMessage(state=False, response_message=str(e))
+    finally:
+        conn.close()
--- Models/Horarios/HorarioModel.py
+++ Models/Horarios/HorarioModel.py
@@ -1,1 +1,24 @@
 
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import date
+
+class HorarioRequest(BaseModel):
+    Fecha_inicio: Optional[date] = None
+    Fecha_final: Optional[date] = None
+    Cedula_emplado: Optional[str] = None
+    Horas_lunes: Optional[int] = None
+    Horas_martes: Optional[int] = None
+    Horas_miercoles: Optional[int] = None
+    Horas_jueves: Optional[int] = None
+    Horas_viernes: Optional[int] = None
+
+class Horario(BaseModel):
+    Fecha_inicio: Optional[date] = None
+    Fecha_final: Optional[date] = None
+    Empleado_id: Optional[int] = None
+    Horas_lunes: Optional[int] = None
+    Horas_martes: Optional[int] = None
+    Horas_miercoles: Optional[int] = None
+    Horas_jueves: Optional[int] = None
+    Horas_viernes: Optional[int] = None
