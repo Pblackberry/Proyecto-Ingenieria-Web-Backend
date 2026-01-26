2bea04bf08294a7673b330d8939c847ca316f30d
Tebas25 <andres.almeida.yanez@udla.edu.ec>
2026-01-25 23:23:39 -0500

    Actualizar permisos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -6,13 +7,24 @@ from Controllers.Administrator.EmpleadoAdministrator import router as empleador_
 from Controllers.Administrator.HorarioAdiministrator import router as horario_router
 from Controllers.Core.EmployeeReportController import router as report_router
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173", 
+    "https://frontend-ingweb.onrender.com"
+]
+
+env_origins = os.getenv("ALLOWED_ORIGINS")
+
+if env_origins:
+    origins.extend(env_origins.split(","))
+
 app.include_router(login_router)
 app.include_router(empleador_router)
 app.include_router(horario_router)
 app.include_router(report_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
